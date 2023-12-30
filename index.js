3aa1a9f5b1e2153778e6bd3f53b41d182aa2ba92
surya-032 <114859751+surya-032@users.noreply.github.com>
2023-12-31 07:12:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello" }');
+    res.send('{ "response": "Hello World" }');
 });
 
 app.get('/will', function (req, res) {
