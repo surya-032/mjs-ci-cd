19460a3486bb0cc1082467b230eadd08db68ee53
surya-032 <114859751+surya-032@users.noreply.github.com>
2023-12-30 21:22:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/will', function (req, res) {
     res.send('{ "response": "implementasi cicd" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": "Berhasil Sukses!!" }');
+    res.send('{ "response": "Berhasil Sukses!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
