edbb866dbf54b8ee2208214dc48db5d8c9d0bfe3
surya-032 <114859751+surya-032@users.noreply.github.com>
2023-12-31 07:10:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello" }')
+    res.send('{ "response": "Hello" }');
 });
 
 app.get('/will', function (req, res) {
     res.send('{ "response": "implementasi cicd" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": "Berhasil Sukses!" }');
+    res.send('{ "response": "Thank you!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
